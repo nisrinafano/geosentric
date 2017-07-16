60d735447019b070dee82207dc59ce1918e94ea3
titisAdi <tmeilaningrum@gmail.com>
2017-07-16 10:49:05 +0700

    home & slideshow
    Home dan slideshow.
    Warna home diupdate dan slideshow sudah otomatis

---
+++ application/config/autoload.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$autoload['packages'] = array();
+$autoload['libraries'] = array('database','session','pagination','upload');
+$autoload['drivers'] = array();
+$autoload['helper'] = array('url','form','file','download','cookie');
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
